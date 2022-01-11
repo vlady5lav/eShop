4467b2dacf0b8111c50e43bcc77d4a814e2e41fa
ShevchenkoVladyslav <48383830+ShevchenkoVladyslav@users.noreply.github.com>
2022-01-11 20:29:01 +0200

    Completed HW

---
+++ eShop/Catalog/Catalog.Host/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Host.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
+}
